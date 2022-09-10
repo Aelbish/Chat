5788dc02c933f5e6d04d1c839481e7ae83bfff6a
Aelbish Shrestha <aelbish@gmail.com>
2022-09-10 04:09:25 -0500

    feat: add recent updates message

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,15 +2,16 @@ import React, { useRef, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
-
 import { useAuth } from "../contexts/AuthContext";
 import axios from "axios";
+import "./Chat.css";
 
 const Chats = () => {
   const didMountRef = useRef(false);
   const history = useHistory();
   const { user } = useAuth();
   const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
 
   const handleLogout = async () => {
     await auth.signOut();
@@ -48,6 +49,7 @@ const Chats = () => {
         })
         //Create new user
         .catch((error) => {
+          setLoadingError(true);
           let formdata = new FormData();
           formdata.append("email", user.email);
           formdata.append("username", user.email);
@@ -60,8 +62,14 @@ const Chats = () => {
                   "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY,
                 },
               })
-              .then(() => setLoading(false))
-              .catch((error) => console.log(error));
+              .then(() => {
+                setLoading(false);
+                setLoadingError(false);
+              })
+              .catch((error) => {
+                setLoadingError(true);
+                console.log(error);
+              });
           });
         });
     }
@@ -69,6 +77,31 @@ const Chats = () => {
       didMountRef.current = false;
     };
   }, [user, history]);
+
+  if (loadingError)
+    return (
+      <>
+        <img
+          src="https://chat-engine-assets.s3.amazonaws.com/temp-logo-min.png"
+          className="logo"
+          width="192"
+        />
+        <h1 className="mainMessage">
+          Unfortunately, my basic plan for Chat Engine has expired.
+        </h1>
+        <p className="secondaryMessage">
+          You can view the source code for this project on{" "}
+          <a href="https://github.com/Aelbish/Chat" className="url">
+            GitHub
+          </a>{" "}
+          or see other projects{" "}
+          <a href="https://aelbish.com/" className="url">
+            here
+          </a>
+          .
+        </p>
+      </>
+    );
 
   if (!user || loading) return "Loading ... ";
 
